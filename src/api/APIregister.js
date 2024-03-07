07a085a8556cddfaacaed0da340d8a8a9635c001
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-07 20:19:27 +0530

    Update APIregister.js

--- src/api/APIregister.js
+++ src/api/APIregister.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const registerapi = async (data) => {
     const response = await axios({
-        url: "http://localhost:80/auth/register",
+        url: "https://borcelle-backend.onrender.com/register",
         headers: { "Content-Type": "application/json" },
         method: "POST",
         data: JSON.stringify(data)
