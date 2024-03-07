72826aa969812fd7ca2be737fbd8b5eceefd1411
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-07 20:19:56 +0530

    Update Getsingle .js

--- src/api/Getsingle .js
+++ src/api/Getsingle .js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Getproducts= async(id)=>{
     const response= await axios({
-        url:`http://localhost:80/products/${id}`,
+        url:`https://borcelle-backend.onrender.com/products/${id}`,
         headers:{"Content-Type":"application/json"},
         method:"GET",
     })
