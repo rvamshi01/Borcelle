3939b017d15679402726b9832d3e4386ae53f595
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-07 20:19:13 +0530

    Update APIlogin.js

--- src/api/APIlogin.js
+++ src/api/APIlogin.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const loginapi= async(data)=>{
     const response= await axios({
-        url:"http://localhost:80/auth/login",
+        url:"https://borcelle-backend.onrender.com/auth/login",
         headers:{"Content-Type":"application/json"},
         method:"POST",
         data:JSON.stringify(data)
